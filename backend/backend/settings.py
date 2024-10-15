b4edf9e32dd4f2ba1d93070cc2882f8155bec39e
Timelaying <makanjuola.timi@gmail.com>
2024-10-15 21:26:43 +0100

    env check

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,7 +15,8 @@ from datetime import timedelta
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+#load_dotenv()
+load_dotenv(dotenv_path=Path(BASE_DIR) / '.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
