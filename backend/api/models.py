5e18a3d061d3d36959e5246397ef629907cc91b3
Timelaying <makanjuola.timi@gmail.com>
2024-10-15 22:54:21 +0100

    model base

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,5 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Note(models.Model):
+    title = models.CharField(max_lenght=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add= True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
 
+    
 # Create your models here.
