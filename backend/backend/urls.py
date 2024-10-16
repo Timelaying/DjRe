9b667bef5caede0715c6fcd2fcc02515ea6f6f87
Timelaying <makanjuola.timi@gmail.com>
2024-10-16 19:30:56 +0100

    including api url path in main url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
-    path("api/token/refresh", TokenRefreshView.as_view(), name="refresh"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
+
+    #link for forwarding specific urls to api
+    path("api/", include("api.urls")), # this means that if the path is not in this file, take api/ and with the rest of the path in api.url
 ]
