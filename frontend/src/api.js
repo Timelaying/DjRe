31f830d4813936aec740a913611e8347034d45c5
Timelaying <makanjuola.timi@gmail.com>
2024-10-17 21:43:43 +0100

    token config plus

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,17 @@
 //writing interceptors(adds corrects header, (checks for tokens)
 
-import axios from axios
+import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 })
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+    }
+)
