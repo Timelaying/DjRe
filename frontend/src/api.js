9107b1607a06bc67dca408cc60f54ba338af39a3
Timelaying <makanjuola.timi@gmail.com>
2024-10-18 11:45:52 +0100

    base url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,8 @@
 //interceptors, intercept request and add their correct headers
+import axios from "axios"
+import { ACCESS_TOKEN } from "./constants"
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
+
