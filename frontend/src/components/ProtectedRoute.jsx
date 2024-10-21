9dbdd794778353487b70e61138d2875bd27476a1
Timelaying <makanjuola.timi@gmail.com>
2024-10-21 10:52:42 +0100

    base Authentication

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -5,5 +5,19 @@ import api from "../api"
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants" 
 
 function ProtectedRoute({children}){
-    
+     const [isAuthorized, setIsAuthorized] = useState(null)
+
+     const refreshToken = async () => {
+
+     }
+
+     const auth = async () => {
+        
+     }
+
+     if (isAuthorized === null) {
+        return <div>Loading...</div>
+     }
+
+     return isAuthorized ? children : <Navigate to ="/login"/>
 }
