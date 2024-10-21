b032be7219bc3c187ad4f3f1af78c5f37936585e
Timelaying <makanjuola.timi@gmail.com>
2024-10-21 11:12:20 +0100

    setting refresh token

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -8,11 +8,27 @@ function ProtectedRoute({children}){
      const [isAuthorized, setIsAuthorized] = useState(null)
 
      const refreshToken = async () => {
-
+        const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+        try {
+            const res = await api.post("api/token/refresh/", {
+                refresh: refreshToken,
+            });
+            if (res.status === 200) {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access)
+                setIsAuthorized(true)
+            } else {
+                setIsAuthorized(false)
+            }
+            
+        } catch (error) {
+            console.log(error);
+            setIsAuthorized(false);
+            
+        }
      }
 
      const auth = async () => {
-        const token = localStorage.getItem{ACCESS_TOKEN}
+        const token = localStorage.getItem(ACCESS_TOKEN)
         if (!token) {
             setIsAuthorized(false)
             return
