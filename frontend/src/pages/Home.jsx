5d2b3b8ad48ad6af2436fd6c325d0305183b526b
Timelaying <makanjuola.timi@gmail.com>
2024-10-25 14:57:24 +0100

    get notes funtion

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,17 @@
-import { useState, useEffect } from "react"
-import api from "../api"
+import { useState, useEffect } from "react";
+import api from "../api";
 
 function Home() {
     const [notes, setNotes] = useState([]);
     const [content, setContent] = useState("")
-    const [title, setTitle] useState("")
+    const [title, setTitle] = useState("")
 
-    const getNote = () => {
+    const getNotes = () => {
         api.get("/api/notes/")
-    }
+        .then((res) => res.data)
+        .then((data) => setNotes(data))
+        .catch((err) => alert(err));
+    };
     
     return <div>Home...</div>
 }
