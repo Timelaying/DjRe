fe81de4d6ca8bf065fc39911435bbc75b706f9f8
Timelaying <makanjuola.timi@gmail.com>
2024-10-25 14:58:15 +0100

    formatting getNotes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,18 +2,19 @@ import { useState, useEffect } from "react";
 import api from "../api";
 
 function Home() {
-    const [notes, setNotes] = useState([]);
-    const [content, setContent] = useState("")
-    const [title, setTitle] = useState("")
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
 
-    const getNotes = () => {
-        api.get("/api/notes/")
-        .then((res) => res.data)
-        .then((data) => setNotes(data))
-        .catch((err) => alert(err));
-    };
-    
-    return <div>Home...</div>
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => setNotes(data))
+      .catch((err) => alert(err));
+  };
+
+  return <div>Home...</div>;
 }
 
-export default Home
+export default Home;
