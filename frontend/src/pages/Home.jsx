b70ae731d4b5da7687806cfd1e01c3118f98103e
Timelaying <makanjuola.timi@gmail.com>
2024-10-25 14:51:22 +0100

    setting useStates

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,11 @@ import { useState, useEffect } from "react"
 import api from "../api"
 
 function Home() {
-    
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState(""
+    const [title, setTitle] useState("")
+
+    )
     return <div>Home...</div>
 }
 
